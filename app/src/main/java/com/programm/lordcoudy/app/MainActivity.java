fb0b87e36ef7cf7596f9ceaa33f58ab64381c0db
lordcoudy <savva.x0712@gmail.com>
2017-11-13 21:45:25 +0300

    Some add

--- app/src/main/java/com/programm/lordcoudy/app/MainActivity.java
+++ app/src/main/java/com/programm/lordcoudy/app/MainActivity.java
@@ -22,10 +22,24 @@ public class MainActivity extends AppCompatActivity {
 
         EditText num2 =
                 (EditText) this.findViewById(R.id.num2);
-        String snum2 = num1.getText().toString();
+        String snum2 = num2.getText().toString();
 
         TextView out =
                 (TextView) this.findViewById(R.id.out);
         out.setText("" + (Double.parseDouble(snum1) + Double.parseDouble(snum2)));
+    }
+    public void minus(View view) {
+        EditText num1 =
+                (EditText) this.findViewById(R.id.num1);
+        String snum1 = num1.getText().toString();
+
+
+        EditText num2 =
+                (EditText) this.findViewById(R.id.num2);
+        String snum2 = num2.getText().toString();
+
+        TextView out =
+                (TextView) this.findViewById(R.id.out);
+        out.setText("" + (Double.parseDouble(snum1) - Double.parseDouble(snum2)));
     }
 }
