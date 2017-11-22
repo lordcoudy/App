3434e3c4f63bf6851c472f500984f160b92c9c1d
lordcoudy <savva.x0712@gmail.com>
2017-11-22 21:30:08 +0300

    Added exceptions

--- app/src/main/java/com/programm/lordcoudy/app/MainActivity.java
+++ app/src/main/java/com/programm/lordcoudy/app/MainActivity.java
@@ -1,11 +1,15 @@
 package com.programm.lordcoudy.app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.EmptyStackException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,63 +21,80 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
     public void plus(View view) {
-        EditText num1 =
-                (EditText) this.findViewById(R.id.num1);
-        double sNum1 = Double.parseDouble(num1.getText().toString());
+        try {
+
+            EditText num1 =
+                    (EditText) this.findViewById(R.id.num1);
+            double sNum1 = Double.parseDouble(num1.getText().toString());
 
 
-        EditText num2 =
-                (EditText)this.findViewById(R.id.num2);
-        double sNum2 = Double.parseDouble(num2.getText().toString());
+            EditText num2 =
+                    (EditText) this.findViewById(R.id.num2);
+            double sNum2 = Double.parseDouble(num2.getText().toString());
 
-        TextView out =
-                (TextView) this.findViewById(R.id.out);
-        out.setText("" + (sNum1 + sNum2));
+            TextView out =
+                    (TextView) this.findViewById(R.id.out);
+            out.setText("" + (sNum1 + sNum2));
+        } catch (NumberFormatException z) {
+            Toast.makeText(this, "Oops! Something wrong!: " + z, Toast.LENGTH_LONG).show();
+        }
     }
+
+
     public void minus(View view) {
-        EditText num1 =
-                (EditText) this.findViewById(R.id.num1);
-        double sNum1 = Double.parseDouble(num1.getText().toString());
+        try {
+            EditText num1 =
+                    (EditText) this.findViewById(R.id.num1);
+            double sNum1 = Double.parseDouble(num1.getText().toString());
 
 
-        EditText num2 =
-                (EditText)this.findViewById(R.id.num2);
-        double sNum2 = Double.parseDouble(num2.getText().toString());
+            EditText num2 =
+                    (EditText) this.findViewById(R.id.num2);
+            double sNum2 = Double.parseDouble(num2.getText().toString());
 
-        TextView out =
-                (TextView) this.findViewById(R.id.out);
-        out.setText("" + (sNum1 - sNum2));
+            TextView out =
+                    (TextView) this.findViewById(R.id.out);
+            out.setText("" + (sNum1 - sNum2));
+        } catch (NumberFormatException z) {
+            Toast.makeText(this, "Oops! Something wrong!: " + z, Toast.LENGTH_LONG).show();
+        }
     }
     public void multiply(View view) {
-        EditText num1 =
-                (EditText) this.findViewById(R.id.num1);
-        double sNum1 = Double.parseDouble(num1.getText().toString());
+        try {
+            EditText num1 =
+                    (EditText) this.findViewById(R.id.num1);
+            double sNum1 = Double.parseDouble(num1.getText().toString());
 
 
-        EditText num2 =
-                (EditText)this.findViewById(R.id.num2);
-        double sNum2 = Double.parseDouble(num2.getText().toString());
+            EditText num2 =
+                    (EditText) this.findViewById(R.id.num2);
+            double sNum2 = Double.parseDouble(num2.getText().toString());
 
-        TextView out =
-                (TextView) this.findViewById(R.id.out);
-        out.setText("" + (sNum1 * sNum2));
+            TextView out =
+                    (TextView) this.findViewById(R.id.out);
+            out.setText("" + (sNum1 * sNum2));
+        } catch (NumberFormatException z) {
+            Toast.makeText(this, "Oops! Something wrong!: " + z, Toast.LENGTH_LONG).show();
+        }
     }
     public void divide(View view) {
-        EditText num1 =
-                (EditText) this.findViewById(R.id.num1);
-        double sNum1 = Double.parseDouble(num1.getText().toString());
+        try {
+            EditText num1 =
+                    (EditText) this.findViewById(R.id.num1);
+            double sNum1 = Double.parseDouble(num1.getText().toString());
 
 
-        EditText num2 =
-                (EditText)this.findViewById(R.id.num2);
-        double sNum2 = Double.parseDouble(num2.getText().toString());
+            EditText num2 =
+                    (EditText) this.findViewById(R.id.num2);
+            double sNum2 = Double.parseDouble(num2.getText().toString());
 
-        TextView out =
-                (TextView) this.findViewById(R.id.out);
-        out.setText("" + (sNum1 / sNum2));
+            TextView out =
+                    (TextView) this.findViewById(R.id.out);
+            out.setText("" + (sNum1 / sNum2));
+        } catch (NumberFormatException z) {
+            Toast.makeText(this, "Oops! Something wrong!: " + z, Toast.LENGTH_LONG).show();
+        }
     }
     public void clear(View view){
         EditText num1 =
@@ -89,8 +110,10 @@ public class MainActivity extends AppCompatActivity {
         out.setText("");
 
     }
-    public void Second(View view){
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void google(View view){
+        //Intent intent = new Intent(this, SecondActivity.class);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://google.ru"));
         startActivity(intent);
     }
 }
--- app/src/main/java/com/programm/lordcoudy/app/SecondActivity.java
+++ app/src/main/java/com/programm/lordcoudy/app/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.programm.lordcoudy.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = this.getIntent();
     }
 }
